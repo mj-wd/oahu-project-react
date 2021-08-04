dd633b293f17f1ced81db1ef48d2af36ba2bcbdc
Mark <86029787+crushahas@users.noreply.github.com>
2021-08-04 15:29:34 -0500

    re-install font awesome & bootstrap-social - it werkz!

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -3,13 +3,7 @@ import React from 'react';
 
 function Footer(props) {
     return (
-        <footer 
-            className="site-footer"
-            // style={{
-            //     backgroundColor: '#61daf',
-            //     color: 'white',
-            // }}
-            >
+        <footer className="site-footer">
             <div className="container">
                 <div className="row">             
                     <div className="col-4 col-sm-2 offset-1">
@@ -25,7 +19,7 @@ function Footer(props) {
                     </div>
                     <div className="col-6 col-sm-3 text-center">
                         <h5>Social Media</h5>
-                        <a className="btn btn-social-icon btn-instagram" href="https://www.instagram.com/gohawaii/?hl=en"><i className="fa fa-instagram" /></a>{' '}
+                        <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i className="fa fa-instagram" /></a>{' '}
                         <a className="btn btn-social-icon btn-facebook" href="https://www.facebook.com/Hawaii/"><i className="fa fa-facebook" /></a>{' '}
                         <a className="btn btn-social-icon btn-twitter" href="https://twitter.com/gohawaii?lang=en"><i className="fa fa-twitter" /></a>{' '}
                         <a className="btn btn-social-icon btn-google" href="https://youtu.be/6XZOgg9WRhI"><i className="fa fa-youtube" /></a> 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
 
 ReactDOM.render(
