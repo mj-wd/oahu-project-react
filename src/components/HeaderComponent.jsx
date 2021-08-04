886bfbaaa8c42638a388882417c9b30c41b3adf2
Zachary Fisher <zacharyfisher808@gmail.com>
2021-08-04 09:22:45 -1000

    added color to navbar
    added color

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Nav, Navbar, NavbarBrand, NavItem, Jumbotron, NavLink, Collapse, NavbarToggler } from 'reactstrap';
 
+
+
 class Header extends Component {
     constructor(props) {
         super(props);
@@ -8,7 +10,6 @@ class Header extends Component {
 
         this.state = {
             isNavOpen: false,
-            isModalOpen: false
         };
         this.toggleNav = this.toggleNav.bind(this);
     }
@@ -33,39 +34,39 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
-                <Navbar dark sticky="top" expand="md" >
+                <Navbar light sticky="top" expand="md" >
                     <div className="container">
                         <NavbarBrand className="mr-auto" href="/"><img src="/assets/oahu-road.jpg" height="30" width="30" alt="Hero" /></NavbarBrand>
                         <NavbarToggler onClick={this.toggleNav} />
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/home">
+                                    <NavLink className="nav-link text-primary" to="/home">
                                         <i className="fa fa-home fa-lg" /> Home
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/Beaches">
+                                    <NavLink className="nav-link text-primary" to="/Beaches">
                                         <i className="fa fa-list fa-lg" /> Beaches
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/Culture">
+                                    <NavLink className="nav-link text-primary" to="/Culture">
                                         <i className="fa fa-address-card fa-lg" /> Culture
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/Geography">
+                                    <NavLink className="nav-link text-primary" to="/Geography">
                                         <i className="fa fa-address-card fa-lg" /> Geography
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/Restaurants">
+                                    <NavLink className="nav-link text-primary" to="/Restaurants">
                                         <i className="fa fa-info fa-lg" /> Restaurants
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/Shopping">
+                                    <NavLink className="nav-link text-primary" to="/Shopping">
                                         <i className="fa fa-address-card fa-lg" /> Shopping
                                     </NavLink>
                                 </NavItem>
