efeac00ac33aab47e02cd2bcf1cd0fcef3d3d795
Zachary Fisher <zacharyfisher808@gmail.com>
2021-08-04 09:04:02 -1000

    added header imports

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavItem, Jumbotron, NavLink } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavItem, Jumbotron, NavLink, Collapse, NavbarToggler } from 'reactstrap';
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+
+
+        this.state = {
+            isNavOpen: false,
+            isModalOpen: false
+        };
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+
+
     render() {
         return (
             <>
@@ -15,50 +33,78 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
-                <Navbar dark>
+                <Navbar dark sticky="top" expand="md" >
                     <div className="container">
-                        <NavbarBrand className="mr-auto" href="/"><img src="/assets/oahu-road.jpg" height="30" width="30" alt="Hero"/></NavbarBrand>
+                        <NavbarBrand className="mr-auto" href="/"><img src="/assets/oahu-road.jpg" height="30" width="30" alt="Hero" /></NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <i className="fa fa-home fa-lg" /> Home
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/Beaches">
+                                        <i className="fa fa-list fa-lg" /> Beaches
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/Culture">
+                                        <i className="fa fa-address-card fa-lg" /> Culture
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/Geography">
+                                        <i className="fa fa-address-card fa-lg" /> Geography
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/Restaurants">
+                                        <i className="fa fa-info fa-lg" /> Restaurants
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/Shopping">
+                                        <i className="fa fa-address-card fa-lg" /> Shopping
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                            
+                        </Collapse>
                     </div>
                 </Navbar>
-                <Nav navbar>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/home">
-                            <i className="fa fa-home fa-lg" /> Home
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/Beaches">
-                            <i className="fa fa-list fa-lg" /> Beaches
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/Culture">
-                            <i className="fa fa-address-card fa-lg" /> Culture
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/Geography">
-                            <i className="fa fa-address-card fa-lg" /> Geography
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/Restaurants">
-                            <i className="fa fa-info fa-lg" /> Restaurants
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/Shopping">
-                            <i className="fa fa-address-card fa-lg" /> Shopping
-                        </NavLink>
-                    </NavItem>
-                </Nav>
+                                {/* <Navbar dark sticky="top" expand="md">
+                    <div className="container">
+                        <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/logo.png" height="30" width="30" alt="NuCamp Logo" /></NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <i className="fa fa-home fa-lg" /> Home
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/directory">
+                                        <i className="fa fa-list fa-lg" /> Directory
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                        <i className="fa fa-info fa-lg" /> About
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <i className="fa fa-address-card fa-lg" /> Contact Us
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </div>
+                </Navbar> */}
             </>
-
-
-
-
-
-
         )
     }
 }
