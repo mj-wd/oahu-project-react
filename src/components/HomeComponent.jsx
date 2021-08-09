1859cbc3ed4a5d43db558707af8d4a7139b1abc9
Mark <86029787+crushahas@users.noreply.github.com>
2021-08-09 10:48:32 -0500

    Added React-Router-Dom

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,51 +1,48 @@
 import React from 'react';
-// import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-// import { Link } from 'react-router-dom';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 
-function Home(props) {
+import { Link } from 'react-router-dom';
+
+function RenderDirectoryItem({campsite}) {
     return (
-            <div className="container">
-                <h6>Site Directory</h6>
-                <ul className="list-unstyled">
-                    <li>Home</li>
-                    <li>Beaches</li>
-                    <li>Culture</li>
-                    <li>Geography</li>
-                    <li>Restaurants</li>
-                    <li>Shopping</li>
-                </ul>
-            </div>                    
+        <Card>
+            <Link to={`/directory/${campsite.id}`}>
+                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
+        </Card>
     );
 }
 
-// function RenderCard({item}) {
-//     return (
-//         <Card>
-//             <CardImg src={item.image} alt={item.name} />
-//             <CardBody>
-//                 <CardTitle>{item.name}</CardTitle>
-//                 <CardText>{item.description}</CardText>
-//             </CardBody>
-//         </Card>
-//     );
-// }
+function Directory(props) {
 
-// function Home(props) {
-//     return (
-//         <div className="container">
-//             <div className="row">
-//                 <div className="col-md m-1">
-//                     <RenderCard item={props.campsite} />
-//                 </div>
-//                 <div className="col-md m-1">
-//                     <RenderCard item={props.promotion} />
-//                 </div>
-//                 <div className="col-md m-1">
-//                     <RenderCard item={props.partner} />
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
+    const directory = props.campsites.map(campsite => {
+        return (
+            <div key={campsite.id} className="col-md-5 m-1">
+                <RenderDirectoryItem campsite={campsite} />
+            </div>
+        );
+    });
 
-export default Home;
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Directory</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Directory</h2>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">
+                {directory}
+            </div>
+        </div>
+    );
+}
+
+export default Directory;
