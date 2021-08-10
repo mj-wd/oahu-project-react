e6bdf35ef3d1b46017d493f4ed589a4a3f984ca2
Mark <86029787+crushahas@users.noreply.github.com>
2021-08-10 12:17:45 -0500

    more modal work

--- src/components/ModalComponent.jsx
+++ src/components/ModalComponent.jsx
@@ -2,44 +2,47 @@ import React from 'react';
 import { BEACHES } from '../shared/beaches';
 import { RESTAURANTS } from '../shared/restaurants'
 import { Modal, ModalHeader, ModalBody, Button, Row, Col} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
-export const initialState = {
-    beaches: BEACHES,
-    restaurants: RESTAURANTS
-}
+// export const initialState = {
+//     beaches: BEACHES,
+//     restaurants: RESTAURANTS
+// }
 
-const onAfterOpen = ({toggler, isOpen, initialState}) => {    
-    // let currentArr = this.initialState;
+function onAfterOpen({toggler, isOpen}) {    
+
     return(
         <Modal className="modal-lg" isOpen={isOpen} toggler={toggler} >
-            <ModalHeader>
-                <Button onClick={toggler}>
-                    X Close
-                </Button>
-                <h2>Our Recommendations</h2>
-            </ModalHeader>
-            <ModalBody>
-                <>
-                    {initialState.map(arr => {
-                        return (
-                            <ul key={arr.id}>
-                                <Row>
-                                    <h3>{arr.name}</h3>
-                                </Row>
-                                <Row>
-                                    <Col md={6}>                
-                                        {arr.image}
-                                    </Col>
-                                    <Col md={6}>       
-                                        <p>{arr.description}</p>
-                                    </Col>
-                                </Row>
-                            </ul>
-                        )
-                    })}
-                </>
-            </ModalBody>
+            {/* <Link to={`/beaches/${BEACHES.id}`}> */}
+                <ModalHeader>
+                    <Button onClick={toggler}>
+                        X Close
+                    </Button>
+                    <h2>Our Recommendations</h2>
+                </ModalHeader>
+                <ModalBody>
+                    <>
+                        {BEACHES.map(arr => {
+                            return (
+                                <ul key={arr.id}>
+                                    <Row>
+                                        <h3>{arr.name}</h3>
+                                    </Row>
+                                    <Row>
+                                        <Col md={6}>                
+                                            {arr.image}
+                                        </Col>
+                                        <Col md={6}>       
+                                            <p>{arr.description}</p>
+                                        </Col>
+                                    </Row>
+                                </ul>
+                            )
+                        })}
+                    </>
+                </ModalBody>
+            {/* </Link> */}
         </Modal>
     );
 };
