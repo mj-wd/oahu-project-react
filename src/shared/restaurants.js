1eac34856b4d6515c66f52447fea1c3339b72972
Mark <86029787+crushahas@users.noreply.github.com>
2021-08-09 19:51:08 -0500

    Create restaurants.js

---
+++ src/shared/restaurants.js
@@ -0,0 +1,26 @@
+export const RESTAURANTS = [
+    {
+        id: 0,
+        name: "Ricados",
+        location: "Pearl Harbor",
+        coordinates: 21.382 -157.940,
+        description: "Italian restaurant",
+        image: "/assets/images/ricados.jpg",
+    },
+    {
+        id: 1,
+        name: "Hanauma Bay",
+        location: "southeast",
+        coordinates: 21.274 -157.694,
+        description: "Idyllic Hanauma Bay, formed in the cone of an extinct volcano, is like a giant aquarium. The park is a very popular snorkeling spot (all visitors need to take a safety and environmental protection lecture before entering), and it really is like snorkeling in a giant aquarium. <br/><br/> To control crowds, attendance is limited. There is an entry charge (well worth it for the experience) and a charge for a little shuttle (also worth it after you're tired from snorkeling all day) to take you up and down the hill from the parking area. (loveoahu.org)",
+        image: "/assets/images/hanauma-bay.jpg",
+    },
+    {
+        id: 2,
+        name: "Lanikai Beach",
+        location: "east",
+        coordinates: 21.393 -157.715,
+        description: " Lanikai also offers beachgoers the full range of water sports, both on top of and under the water. You can sail, windsurf, sea kayak, and snorkel. Kayakers can even paddle out to small uninhabited islands about a mile offshore. The water is warm, clear, and gentle (it's protected by an offshore reef), and the sand is white and very fine. (loveoahu.org)",
+        image: "/assets/images/lanikai-beach.jpg",
+    },
+];
