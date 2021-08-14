3727cc42c0a6fddd582b451a2cbdd650b4c7e51f
Mark <86029787+crushahas@users.noreply.github.com>
2021-08-14 10:55:15 -0700

    Welcome to Oah'u

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -25,7 +25,7 @@ class Header extends Component {
                     <div className="container">
                         <div className="row">
                             <div className="col">
-                                <h1>Hi this is an update</h1>
+                                <h1>Welcome to Oah'u</h1>
                                 <h2>Find out about all the great things this island has to offer!</h2>
                                 <img src="/assets/images/icon.png" width="50" height="50" alt="icon" />
                             </div>
